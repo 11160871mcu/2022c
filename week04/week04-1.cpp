534dcd2a30199c897f0cfdf0a493e9f1d5fa8cb5
11160871mcu <emily0330wu@gmail.com>
2023-03-10 11:43:16 +0800

    week04

---
+++ week04/week04-1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a=10;
+    printf("a的值是%d\n",a);
+    printf("a的在家是%d\n",&a);
+
+    int b=20;
+    printf("b的值是%d\n",b);
+    printf("b的在家是%d\n",&b);
+
+    int c=30;
+    printf("c的值%d\n",c);
+    printf("c的在家是%d\n",&c);
+}
